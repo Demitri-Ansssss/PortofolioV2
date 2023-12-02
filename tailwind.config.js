d2cdb9c53c008e6ec35b7b20fa9363bb3d95b8d9
Demitri-Ansssss <ansoricahgreges@gmail.com>
2023-12-02 20:31:09 +0700

    first Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#86a789",
+        secondary: "#b2c8ba",
+        third: "#d2e3c8",
+        fourth: "#ebf3e8",
+        text: "#c19970",
+        text2: "#fefae0",
+      },
+    },
+  },
+  plugins: [],
+};
